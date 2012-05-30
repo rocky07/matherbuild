bb6ecdb6097852f2193db5870f3069aaacf9fe17
rakesh ramesan <rakesh@forsfortis.net>
2012-05-30 23:24:41 +0530

    removed sme files

--- data/projectgallery.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "[{html:'images/1.jpg',xtype:'panel'},{html:'images/2.jpg',xtype:'panel'},{html:'images/3.jpg',xtype:'panel'},{html:'images/4.jpg',xtype:'panel'},{html:'images/5.jpg',xtype:'panel'}]";
-?>
--- imagegallery.php
+++
@@ -1,8 +0,0 @@
-<?php
-ob_start();
-include("autoload.php");
-
-$util = new Utils();
-$records=$util->listGalleryImages(1);
-echo json_encode($records);
-?>
